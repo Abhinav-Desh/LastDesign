5ae592a31e4d5869180ed92a0f02a3241e7c8548
abhinav-desh <abhinav.deshmukh@pramericalife.in>
2025-01-25 01:43:59 +0530

    add first table view

--- lastdesign/src/components/TableView.tsx
+++ lastdesign/src/components/TableView.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import Spotify from '../Assets/spotify.png'
+import Spotify from '../Assets/spotify.png';
+import { green } from '@mui/material/colors';
+
 const Table1 = [
   {
     Name: "John Michael",
@@ -61,27 +63,26 @@ const TableView = () => {
             <table>
               <thead>
                 <tr className='table-main-header'>
-                  <th className='author-column'>Author</th>
-                  <th className='author-function'>Function</th>
-                  <th className='author-status'>Status</th>
-                  <th className='author-date'>Employed</th>
-                  <th className='action'>Action</th>
+                  <th className='author-column' style={{ width: '30%',textAlign:'start' }}>Author</th>
+                  <th className='author-function' style={{ width: '15%',textAlign:'start' }}>Function</th>
+                  <th className='author-status' style={{ width: '15%',textAlign:'start' }}>Status</th>
+                  <th className='author-date' style={{ width: '20%',textAlign:'start' }}>Employed</th>
+                  <th className='action' style={{ width: '20%',textAlign:'start' }}>Action</th>
                 </tr>
               </thead>
               <tbody>
                 {Table1.map((row, index) => (
-                  <tr key={index}>
-                    <td>
+                  <tr key={index} className='rows-head'>
+                    <td className='author-main'>
                       <div className="author-container">
-                    <div className='author-image'>
-                      <img src={Spotify} alt="" />
+                        <div className='author-image'>
+                          <img src={Spotify} alt="" style={{ width: '30px', height: '30px', borderRadius: '50%' }} />
+                        </div>
+                        <div className='author-name-email'>
+                          <div className='author-name'>{row.Name}</div>
+                          <div className='author-email'>{row.Email}</div>
+                        </div>
                       </div>
-                    <div className='author-name-email'>
-                    <div className='author-name'>{row.Name}</div>
-                    <div className='author-email'>{row.Email}</div>
-                    </div>
-                    </div>
-                    
                     </td>
                     <td className='author-function'>
                       <div className='function-function'>{row.Function}</div>
@@ -90,15 +91,23 @@ const TableView = () => {
                     <td className='status-check'>
                       <span
                         style={{
-                          color: row.Status === 'online' ? 'green' : 'red',
+                          color: '#fff',
+                          background: row.Status === 'online'
+                            ? 'linear-gradient(195deg, rgb(102, 187, 106), rgb(67, 160, 71))'
+                            : 'linear-gradient(195deg, rgb(66, 66, 74), rgb(25, 25, 25))',
+                          fontSize: '12px',
+                          fontWeight: '600',
+                          padding: '2px 10px',
+                          borderRadius: '5px',
                         }}
+                        className='status-button'
                       >
-                        {row.Status}
+                        {row.Status.toUpperCase()}
                       </span>
                     </td>
-                    <td>{row.Employed}</td>
+                    <td className='employed'>{row.Employed}</td>
                     <td>
-                      <button className='Edit'>Edit</button>
+                      <button className='Edit' style={{ padding: '6px 12px'}}>Edit</button>
                     </td>
                   </tr>
                 ))}
@@ -106,7 +115,21 @@ const TableView = () => {
             </table>
           </div>
         </div>
-        <div className="table-2-container">table2</div>
+        <div className="table-2-container">
+          tabletable
+          table
+          table
+          table
+          table
+          table
+          table
+          table
+          table
+          table
+          table
+          table
+          tabletable
+        </div>
       </div>
     </div>
   );
